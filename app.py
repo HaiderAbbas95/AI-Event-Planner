164b129911ddd15aa501eade80385e9295b3ce69
HaiderAbbas95 <malikhaider.1396@gmail.com>
2025-06-05 22:49:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import sys
 import streamlit as st
   # adjust path if needed
-from Agents.orchestrator import orchestrated_event_plan
-from Agents.user_intent_agent import extract_user_intent
+from orchestrator import orchestrated_event_plan
+from user_intent_agent import extract_user_intent
 
 st.set_page_config(page_title="AI Event Manager", page_icon="🎉")
 st.title("🎉 AI Event Manager")
